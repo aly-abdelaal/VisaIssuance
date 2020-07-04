75496ea05434e1e79ff31397860e014cfa66a7fc
EMARATECH\ali.abdelaal <ali.abdelaal@emmaratech.ae>
2020-07-04 22:12:32 +0400

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFound404Component } from './components/not-found404/not-found404.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:  HomeComponent},
+  { path: '**', component: NotFound404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
