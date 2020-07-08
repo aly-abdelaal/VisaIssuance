d7c7d8b8c0ffd57d618c7d68b28f7eda4529890f
aly-abdelaal <ali.abd-elaal@live.com>
2020-07-08 09:19:51 +0400

    add fees grid

--- src/app/components/application-details/application-details.component.ts
+++ src/app/components/application-details/application-details.component.ts
@@ -8,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class ApplicationDetailsComponent implements OnInit {
 
   isSearch = true;
+  fees: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.addFees();
+  }
+
+  addFees(): void {
+    this.fees.push(
+      {
+        id: '1',
+        name: 'Applicattion fee',
+        amount: 150
+      },
+      {
+        id: '2',
+        name: 'Knowledge fee',
+        amount: 10
+      },
+      {
+        id: '3',
+        name: 'Issunace fee',
+        amount: 100
+      },
+      {
+        id: '4',
+        name: 'Inside the country fee',
+        amount: 650
+      }
+    );
   }
 
 }
